f7d97a83ca73b5eb0fb595c00884b72dd57f8b58
Sofurs <matej@las.sk>
2022-09-29 22:58:02 +0200

    config changes

--- config.h
+++ config.h
@@ -3,11 +3,11 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-network", 5,    24),
-	BLOCK("sb-battery", 5,    25),
-	BLOCK("sb-volume",  0,    5),
-	BLOCK("sb-date",    1,    27)
+	BLOCK("dblocks-disk",    1800, 19),
+	BLOCK("dblocks-memory",  10,   20),
+	BLOCK("dblocks-load", 5,    21),
+	BLOCK("dblocks-network", 5,    24),
+	BLOCK("dblocks-battery", 5,    25),
+	BLOCK("dblocks-volume",  0,    5),
+	BLOCK("dblocks-date",    1,    27)
 };
